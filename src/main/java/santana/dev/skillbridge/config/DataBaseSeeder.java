4cdb0d51595f9198192d225cfabff564dd8a6776
FelipeSants08 <felipe_santana.01@hotmail.com>
2025-11-21 11:24:52 -0300

    Alterando o banco de dados para Postgres

--- src/main/java/santana/dev/skillbridge/config/DataBaseSeeder.java
+++ src/main/java/santana/dev/skillbridge/config/DataBaseSeeder.java
@@ -34,41 +34,7 @@ public class DataBaseSeeder {
                 .role(UserRole.USER)
                 .build();
         userRepository.saveAll(List.of(yasmin,felipe));
-//        List<Skill> skills = List.of(
-//                Skill.builder().skillName("Java").category("Backend").build(),
-//                Skill.builder().skillName("Spring Boot").category("Backend").build(),
-//                Skill.builder().skillName("Python").category("Backend").build(),
-//                Skill.builder().skillName("Django").category("Backend").build(),
-//                Skill.builder().skillName("Node.js").category("Backend").build(),
-//                Skill.builder().skillName("Express.js").category("Backend").build(),
-//
-//                Skill.builder().skillName("JavaScript").category("Frontend").build(),
-//                Skill.builder().skillName("TypeScript").category("Frontend").build(),
-//                Skill.builder().skillName("React").category("Frontend").build(),
-//                Skill.builder().skillName("Angular").category("Frontend").build(),
-//                Skill.builder().skillName("Vue.js").category("Frontend").build(),
-//                Skill.builder().skillName("HTML").category("Frontend").build(),
-//                Skill.builder().skillName("CSS").category("Frontend").build(),
-//
-//                Skill.builder().skillName("SQL").category("Database").build(),
-//                Skill.builder().skillName("PostgreSQL").category("Database").build(),
-//                Skill.builder().skillName("MySQL").category("Database").build(),
-//                Skill.builder().skillName("Oracle").category("Database").build(),
-//                Skill.builder().skillName("MongoDB").category("Database").build(),
-//
-//                Skill.builder().skillName("Docker").category("DevOps").build(),
-//                Skill.builder().skillName("Kubernetes").category("DevOps").build(),
-//                Skill.builder().skillName("Git").category("DevOps").build(),
-//                Skill.builder().skillName("Linux").category("DevOps").build(),
-//                Skill.builder().skillName("CI/CD").category("DevOps").build(),
-//
-//                Skill.builder().skillName("Scrum").category("Methodology").build(),
-//                Skill.builder().skillName("Kanban").category("Methodology").build(),
-//                Skill.builder().skillName("Agile").category("Methodology").build(),
-//                Skill.builder().skillName("Comunicação").category("Soft Skill").build(),
-//                Skill.builder().skillName("Trabalho em equipe").category("Soft Skill").build()
-//        );
-//        skillRepository.saveAll(skills);
+
 
     }
 }
